d6d0c77b180d6e1f845c16206b6e2425f134da8f
Kat Le <kl2853@barnard.edu>
2019-08-15 12:08:00 -0400

    wrap doesnt even work dumdums

--- wrap.js
+++ wrap.js
@@ -1,17 +1,46 @@
 /* eslint-disable no-undef */
 
-const wrap = (line, maxLen) => {
-  let newString = '';
-  if (line.length <= maxLen) {
+// const wrap = (line, maxLen) => {
+//   let newString = '';
+//   if (line.length <= maxLen) {
+//     return line;
+//   }
+//   else {
+//     // newString = line.slice(0, maxLen) + '\n' + line.slice(maxLen);
+//     line = line.split(' ');
+//     let max = maxLen
+//     for(let i = 0; i < line.length; i++) {
+//       if(newString.length + line[i].length > max) {
+//         newString += '\n';
+//         newString += line[i]
+//         max = max + max;
+//       }
+//       else {
+//         newString += line[i] + ' '
+//       }
+//     }
+//   }
+//   console.log(newString);
+//   return newString;
+// };
+
+function wrap(line, maxLen) {
+  if(line.length <= maxLen) {
     return line;
   }
 
-  return newString;
-};
+  const indexOfBlank = line.lastIndexOf(' ', maxLen);
+  let split;
+  let offset;
+  if(indexOfBlank > -1) {
+    split = indexOfBlank;
+    offset = 1;
+  } else {
+    split = maxLen;
+    offset = 0;
+  }
+
+  return line.substring(0, split) + '\n' + wrap(line.substring(split + offset), maxLen);
+}
 
 module.exports = wrap;
-
-console.log('Lorem ipsum dolor'.length);
-console.log('sit eu amet, elit na'.length);
-console.log('magna sem amet nulla'.length);
-console.log('vel purus ac ligula'.length);
--- wrap.spec.js
+++ wrap.spec.js
@@ -6,19 +6,20 @@ describe('wrap', () => {
     expect(wrap('', 10)).to.equal('');
   });
 
-  //   it('Takes in two arguments', () => {
-  //     expect(wrap('', 10)).to.equal('');
-  //   });
-
   it('Returns the entire string if the string length <= maxLen', () => {
     expect(wrap('This string is smol', 30)).to.equal('This string is smol');
   });
 
   it('inserts a new line at or before maxLen if the string length > maxLen', () => {
-    expect(wrap('This string', 5)).to.equal('This \nstring');
+    expect(wrap('This string', 5)).to.equal('This\nstring');
   });
 
-  //   it('Returns a string of maxLen before/between new line', () => {
-  //     expect(wrap('', 10)).to.equal('');
-  //   });
+  it('does not separate whole words', () => {
+    expect(wrap('Dog days', 6)).to.equal('Dog\ndays');
+  });
+
+  it('can wrap whole', () => {
+    expect(wrap("Lorem ipsum dolor sit eu amet, elit na magna sem amet nulla vel purus ac ligula.", 20)).to.equal(`Lorem ipsum dolor\nsit eu amet, elit na\nmagna sem amet nulla\nvel purus ac ligula.`);
+  });
+
 });
